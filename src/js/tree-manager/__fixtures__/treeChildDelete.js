3f95947d8ae9ec2187a887397d597252beb23c02
Andris Sīlis <Andris.Silis@gmail.com>
2015-03-14 01:43:35 +0200

    Removed immstruct which works like Singleton from tests

--- src/js/tree-manager/__fixtures__/treeChildDelete.js
+++ src/js/tree-manager/__fixtures__/treeChildDelete.js
@@ -1,38 +1,35 @@
 jest.autoMockOff();
 
 import { List, Map } from 'immutable';
-import immstruct from 'immstruct';
 
 import TreeNode from '../../tree/TreeNode';
 
 jest.autoMockOn();
 
-export default immstruct({
-  tree: TreeNode({
-    id: 'root',
-    text: 'Root',
-    children: List([
-      TreeNode({
-        id: 'child-1',
-        text: 'child-1',
-      }),
-      TreeNode({
-        id: 'child-2',
-        text: 'child-2'
-      })
-    ])
-  }),
 
-  treeAfterChildDelete: TreeNode({
-    id: 'root',
-    text: 'Root',
-    children: List([
-      TreeNode({
-        id: 'child-1',
-        text: 'child-1',
-      })
-    ])
-  })
+export var tree = TreeNode({
+  text: 'Root',
+  children: List([
+    TreeNode({
+      text: 'child-1',
+    }),
+    TreeNode({
+      text: 'child-2',
+      children: List([
+        TreeNode({
+          text: 'child-2-1',
+        })
+      ])
+    })
+  ])
 });
 
 
+export var treeAfterChildDelete = TreeNode({
+  text: 'Root',
+  children: List([
+    TreeNode({
+      text: 'child-1',
+    })
+  ])
+});
--- src/js/tree-manager/__tests__/tree-manager-test.js
+++ src/js/tree-manager/__tests__/tree-manager-test.js
@@ -1,22 +1,23 @@
 jest.autoMockOff();
 
+import Cursor from 'immutable/contrib/cursor';
 import { deleteChildNode } from '../treeManager';
-import treeChildDeleteFixture from '../__fixtures__/treeChildDelete';
+import { tree, treeAfterChildDelete } from '../__fixtures__/treeChildDelete';
 
 jest.autoMockOn();
 
 
 describe('Tree Manager', () => {
-  it('deletes child from parent children Map', () => {
-    var treeCursor = treeChildDeleteFixture.cursor(['tree']);
+  it("deletes child from parent's children Map", () => {
+    var treeCursor = Cursor.from(tree, newTree => {
+      tree = newTree;
+    });
     var childNode2Cursor = treeCursor.getIn(['children', 1]);
 
     deleteChildNode(treeCursor, childNode2Cursor);
 
     expect(
-      treeChildDeleteFixture
-        .cursor(['tree'])
-        .equals(treeChildDeleteFixture.cursor(['treeAfterChildDelete']))
+      tree.equals(treeAfterChildDelete)
     ).toBe(true);
   });
 });
