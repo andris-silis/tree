2e0261f8206cce3b01719301595cd817a7fecfcf
Andris Sīlis <Andris.Silis@gmail.com>
2015-03-14 02:03:49 +0200

    Renamed fixture

--- src/js/tree-manager/__fixtures__/trees.js
+++ src/js/tree-manager/__fixtures__/trees.js
@@ -33,3 +33,24 @@ export var treeAfterChildDelete = TreeNode({
     })
   ])
 });
+
+
+export var treeAfterChildAdd = TreeNode({
+  text: 'Root',
+  children: List([
+    TreeNode({
+      text: 'child-1',
+    }),
+    TreeNode({
+      text: 'child-2',
+      children: List([
+        TreeNode({
+          text: 'child-2-1',
+        })
+      ])
+    }),
+    TreeNode({
+      text: 'child-3',
+    }),
+  ])
+});
