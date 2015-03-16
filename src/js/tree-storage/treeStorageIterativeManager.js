f150a5ea9bbba24a41cdd958fb57c8993debba5d
Andris Sīlis <Andris.Silis@gmail.com>
2015-03-16 15:25:41 +0200

    Quick and dirty iterative tree data loader and saver

--- src/js/tree-storage/treeStorageIterativeManager.js
+++ src/js/tree-storage/treeStorageIterativeManager.js
@@ -1,28 +1,81 @@
 import { fromJS as immutableFromJS } from 'immutable';
 
-const LOCALSTORAGE_KEY = 'treeDataIterative';
+const LOCALSTORAGE_KEY_PREFIX = 'treeDataIterative';
+
+import { getStorageKey, clearStorage, getAllStorageKeys } from './treeStorageManagerHelpers';
 
 
-var saveTreeData = function (tree) {
-  // No exception handling, sorry :)
+var _saveAllNodes = function (node) {
+  var stack = [node];
+  var i = 0;
+  var currentNode;
+
+  while (currentNode = stack[i]) {
+    i++;
+    _saveSingleNode(currentNode);
+
+    if (currentNode.children && _.isArray(currentNode.children)) {
+      currentNode.children.forEach(childNode => stack.push(childNode));
+    }
+  }
+};
+
+
+var _saveSingleNode = function (data) {
+  var dataToSave = _.omit(data, 'children');
+  dataToSave.children = data.children.map(node => node.id);
+
   localStorage.setItem(
-    LOCALSTORAGE_KEY,
-    JSON.stringify(tree)
+    getStorageKey(LOCALSTORAGE_KEY_PREFIX, data.id),
+    JSON.stringify(dataToSave)
   );
 };
 
 
-var loadTreeData = function () {
-  var localStorageData = localStorage.getItem(LOCALSTORAGE_KEY);
+var _loadNode = function (key) {
+  var localStorageData = localStorage.getItem(
+    key
+  );
 
   if (!localStorageData) {
     return;
   }
 
-  var treeDataObject = JSON.parse(localStorageData);
+  localStorageData = JSON.parse(localStorageData);
+
+
+  return localStorageData;
+};
+
+
+var _loadAllNodes = function () {
+  var allNodes = getAllStorageKeys(LOCALSTORAGE_KEY_PREFIX).map(_loadNode);
+  // Iterates over allNodes and converts
+  //  its children array IDs to instances
+  allNodes = allNodes.map(node => {
+    node.children = node.children.map(
+      childID => _.find(allNodes, found => found.id === childID)
+    );
+    return node;
+  });
+
+  return _.find(allNodes, node => node.id === 'root');
+};
+
+
+var saveTreeData = function (tree) {
+  clearStorage(LOCALSTORAGE_KEY_PREFIX);
+  _saveAllNodes(tree.toJSON())
+};
+
+
+var loadTreeData = function () {
+  var treeDataObject = _loadAllNodes();
   if (!treeDataObject) {
     return;
   }
+
+  treeDataObject = treeDataObject.children;
 
   return immutableFromJS(treeDataObject);
 };
